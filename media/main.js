c7623fd21f53b7774125e8b700e5d42ab4367eaa
Shiv Patel <shivpatelssp@gmail.com>
2020-12-19 13:41:23 -0500

    display ... in snippet preview if exceeds max visible lines

--- media/main.js
+++ media/main.js
@@ -67,9 +67,16 @@
     }
 
     function getSnippetPreview(snippet = '', maxCharsToParse = 500, maxLines = 3) {
-        const subSnippet = (escapeHTML(snippet)).substring(0, maxCharsToParse);
-        const lines = subSnippet.split('\n').slice(0, maxLines).map(part => part.replace(/ /g, '&nbsp;').replace(/\t/g, '&nbsp;&nbsp;'));
-        return lines.reduce((html, line) => html + `<code>${line}</code>`, '');
+        // for efficiency, only look at maxCharsToParse
+        //  then escape HTML carrots
+        //  then split by newline
+        const subSnippetLines = escapeHTML(snippet.substring(0, maxCharsToParse)).split('\n');
+        // get up to max lines and replace spaces and tabs with &nbsp;
+        const previewLines = subSnippetLines.slice(0, maxLines).map(part => part.replace(/ /g, '&nbsp;').replace(/\t/g, '&nbsp;&nbsp;'));
+        // if snippet had more lines than allowed to display, add a preview line with ...
+        if (subSnippetLines.length > maxLines) previewLines.push('...');
+        // return each line wrapped inside <code> element
+        return previewLines.reduce((html, line) => html + `<code>${line}</code>`, '');
     }
 
     function renderSnippets(snippets) {
@@ -119,7 +126,6 @@
     // Handle messages sent from the extension to the webview
     window.addEventListener('message', event => {
         const message = event.data; // The json data that the extension sent
-        console.log(message);
         switch (message.type) {
             case 'render-blocked-users':
                 {
